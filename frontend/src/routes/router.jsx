b9ba2bb4b20b615ca700d82573d8c21dbc9bb0a2
Lorena <lorenaferreira98@outlook.com>
2025-09-15 21:29:27 -0300

    EV-40 Continuação da tela de Esqueci minha senha

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -5,9 +5,11 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import ResetPassword from "../pages/ResetPasswordPage";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
+  { path: "/reset-password", element: <ResetPassword /> }
 ]);
