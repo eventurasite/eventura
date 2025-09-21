b69d5544df242fc2865d2e9ed27a5813534069c3
Victor Santos <126369803+Victor926@users.noreply.github.com>
2025-09-21 16:07:56 -0300

    Correção do router.jsx

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -6,31 +6,16 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Reset_Password from "../pages/ResetPassword";
-
 import AdminDashboard from "../pages/AdminDashboard";
-
-// import do nosso componente de proteção
-import ProtectedRoute from "../components/ProtectedRoute";
-
 import ForgotPassword from "../pages/ForgotPassword";
-
-import Reset_Password from "../pages/ResetPassword";
-
-import AdminDashboard from "../pages/AdminDashboard";
-
 // import do nosso componente de proteção
 import ProtectedRoute from "../components/ProtectedRoute";
-
 
 export const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
-
-
   { path: "/forgotpassword", element: <ForgotPassword /> },
-
-
   { path: "/resetpassword", element: <Reset_Password /> },
   
   {
